ff70c4151b4b2c3c15e9f47799b6dae9061ef697
KirupaKrishan <kirupamgr@gmail.com>
2026-03-01 18:55:42 +0100

    Added Forward Kine to Arm

--- arm_movement/arm_movement/forward_kine.py
+++ arm_movement/arm_movement/forward_kine.py
@@ -16,17 +16,28 @@ class ForwardKinematicsNode(Node):
             10
         )
         self.subscription  # prevent unused variable warning
-        self.robot = placo.RobotWrapper("arm_movement/Viz_asset/SO101/so101_new_calib.urdf")
+        self.last_joint_angles = None
+        self.robot = placo.RobotWrapper("src/arm_movement/Viz_asset/SO101/so101_new_calib.urdf",placo.Flags.ignore_collisions)
 
     def joint_state_callback(self, msg):
         # Extract joint angles from the message
         joint_names = msg.name
         joint_angles = msg.position
+        if self.last_joint_angles == joint_angles:
+            return
+        else:
+            self.last_joint_angles = joint_angles
+        
         if len(joint_angles) < 6:
             self.get_logger().error('Received joint state with insufficient angles')
             return
-        
-        # Compute forward kinematics
+        for name, q in zip(joint_names, joint_angles):
+            try:
+                self.robot.set_joint(name, float(q))  # common pattern
+            except Exception:
+                self.get_logger().error(f'Failed to set joint {name} to angle {q}')
+        self.robot.update_kinematics()
+        # Update the robot's kinematics with the new joint angles
         ee_position, ee_orientation = self.forward_kinematics(joint_angles)
         
         # Log the end-effector position
@@ -35,12 +46,11 @@ class ForwardKinematicsNode(Node):
 
     def forward_kinematics(self, joint_angles):
         self.get_logger().info(f'Computing forward kinematics for joint angles: {joint_angles}')
-        self.get_logger().info(f'Loading robot model: {self.robot}')
+    
         robot_Tf_matrix = self.robot.get_T_a_b('base_link', 'gripper_frame_link')
         self.get_logger().info(f'Robot transformation matrix: {robot_Tf_matrix}')
         ee_position = robot_Tf_matrix[:3, 3]
         ee_orientation = R.from_matrix(robot_Tf_matrix[:3, :3]).as_euler('xyz')
-        self.robot.update_kinematics()
         return (ee_position, ee_orientation)
 
 def main(args=None):
